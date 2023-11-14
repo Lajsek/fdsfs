27fba08f6db9bd281580448b49c632ff786f46e7
Maciej <maciejnawrot.it@gmail.com>
2023-11-14 12:19:16 +0100

    Initial Commit

---
+++ fdsfs/fdsfsApp.swift
@@ -0,0 +1,17 @@
+//
+//  fdsfsApp.swift
+//  fdsfs
+//
+//  Created by student on 14/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct fdsfsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
